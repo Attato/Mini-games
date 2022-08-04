518c8d1a168b8c5800f4e46111b5f04a16d1a12c
KoffeMeow <guyden999@mail.ru>
2022-08-04 18:41:41 +0300

    tabs fix

--- pages/api/tabs.tsx
+++ pages/api/tabs.tsx
@@ -2,47 +2,58 @@ import React from 'react';
 import Head from 'next/head';
 
 const MemoryGame = (
-	<div className="body" key={Date.now()}>
-            <Head>
-				<title>Memory Game</title>
-				<meta name="description" content="" />
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
+	<>
+		<Head>
+			<title>Memory Game</title>
+			<meta name="description" content="" />
+			<link rel="icon" href="/favicon.ico" />
+		</Head>
+
 		<h1>Memory Game</h1>
-	</div>
+		<div className="body" key={Date.now()}>
+
+		</div>
+	</>
 );
 
 const Snake = (
-	<div className="body" key={Date.now()}>
-        <Head>
-            <title>Snake</title>
-            <meta name="description" content="" />
-            <link rel="icon" href="/favicon.ico" />
-        </Head>
-		<h1>Snake</h1>
-	</div>
+	<>
+		<Head>
+			<title>Snake</title>
+			<meta name="description" content="" />
+			<link rel="icon" href="/favicon.ico" />
+		</Head>
+
+		<div className="body" key={Date.now()}>
+			<h1>Snake</h1>
+		</div>
+	</>
 );
 
 const Chess = (
-	<div className="body" key={Date.now()}>
-        <Head>
-            <title>Chess</title>
-            <meta name="description" content="" />
-            <link rel="icon" href="/favicon.ico" />
-        </Head>
-		<h1>Chess</h1>
-	</div>
+	<>
+		<Head>
+			<title>Chess</title>
+			<meta name="description" content="" />
+			<link rel="icon" href="/favicon.ico" />
+		</Head>
+		<div className="body" key={Date.now()}>
+			<h1>Chess</h1>
+		</div>
+	</>
 );
 
 const BossFight = (
-	<div className="body" key={Date.now()}>
-        <Head>
-            <title>Boss fight</title>
-            <meta name="description" content="" />
-            <link rel="icon" href="/favicon.ico" />
-        </Head>
-		<h1>Boss fight</h1>
-	</div>
+	<>
+		<div className="body" key={Date.now()}>
+			<Head>
+				<title>Boss fight</title>
+				<meta name="description" content="" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<h1>Boss fight</h1>
+		</div>
+	</>
 );
 
 export const tabs = [
